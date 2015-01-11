19864e630e949e5be7f9d3361340933929809ae2
Paul <paul.lugagnedelpon@gmail.com>
2015-01-12 00:01:54 +0800

    Load user avatar on S3.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, 
+  has_attached_file :avatar,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials },
                     :styles => { :medium => "300x300>", :thumb => "100x100>" },
-                    :default_url => "/images/:style/missing.png"
+                    :default_url => "https://s3.amazonaws.com/predikt-app/missing.jpg"
 
   validates_attachment_content_type :avatar, 
                                     :content_type => /\Aimage\/.*\Z/
 
+  def s3_credentials
+    {:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
+  end
 end
