bf27c98d2d6471222ff8e6e57575fa14239f4241
Paul <paul.lugagnedelpon@gmail.com>
2015-01-18 01:06:48 +0800

    Change name of secret codes in .env

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,10 @@ class User < ActiveRecord::Base
   # validates :username, uniqueness: true
 
   def s3_credentials
-    {:bucket => ENV['S3_BUCKET'], :access_key_id => ENV['ACCESS_KEY_ID'], :secret_access_key => ENV['SECRET_ACCESS_KEY']}
+    {
+      :bucket => ENV['S3_BUCKET'],
+      :access_key_id => ENV['S3_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['S3_SECRET_ACCESS_KEY']
+    }
   end
 end
